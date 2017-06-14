662d8b83249b0ee0252658d546d885502588864f
ZeroFox34 <jbluper@outlook.com>
2017-06-14 19:18:30 -0400

    Initial?

---
+++ ProjectDooder/ProjectDooder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectDooder.Startup))]
+namespace ProjectDooder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
